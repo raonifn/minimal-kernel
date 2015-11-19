d6b14e22a241487a7bfc7e4529b5c3bff8428e0e
Raoni Normanton <raonifn@gmail.com>
2015-11-19 16:47:42 -0200

    memory paging

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -131,7 +131,9 @@ test_long_mode:
 section .rodata
 gdt64:
     dq 0 ; zero entry
+.code: equ $ - gdt64 ; new
     dq (1<<44) | (1<<47) | (1<<41) | (1<<43) | (1<<53) ; code segment
+.data: equ $ - gdt64 ; new
     dq (1<<44) | (1<<47) | (1<<41) ; data segment
 .pointer:
     dw $ - gdt64 - 1
