777a357e642aa754cb372b675c7a8a7b0d6839e0
Raoni Normanton <raonifn@gmail.com>
2016-04-27 11:51:18 -0300

    os

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,42 @@
 #![feature(lang_items)]
+#![feature(const_fn)]
+#![feature(unique)]
 #![no_std]
 
+mod vga_buffer;
 extern crate rlibc;
 
 #[no_mangle]
 pub extern fn rust_main() {
-  let x = ["Hello", " ", "World", "!"];
-  let test = (0..3).flat_map(|x| 0..x).zip(0..);
+    // ATTENTION: we have a very small stack and no guard page
 
-  let mut a = ("hello", 42);
-  a.1 += 1;
+    let hello = b"Hello World!";
+    let color_byte = 0x1f; // white foreground, blue background
+
+    let mut hello_colored = [color_byte; 24];
+    for (i, char_byte) in hello.into_iter().enumerate() {
+        hello_colored[i*2] = *char_byte;
+    }
+
+    // write `Hello World!` to the center of the VGA text buffer
+    let buffer_ptr = (0xb8000 + 1988) as *mut _;
+    unsafe { *buffer_ptr = hello_colored };
+
+    loop{}
 }
 
 #[lang = "eh_personality"]
 extern fn eh_personality() {}
 
 #[lang = "panic_fmt"]
-extern fn panic_fmt() -> ! {loop{}}
+extern fn panic_fmt() -> ! {
+
+  let buffer_ptr = (0xb8000) as *mut _;
+  let red = 0x4f;
+  unsafe {
+    *buffer_ptr = [b'P', red, b'a', red, b'n', red, b'i', red, b'c', red, b'!', red];
+  };
+
+
+  loop{}
+}
